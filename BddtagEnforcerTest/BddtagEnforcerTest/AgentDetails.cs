efdeab3dac14c61c2969bb4f21d645cccc96b61b
abhishekch85 <mail2abhishekch@gmail.com>
2023-09-17 20:12:54 +0530

    Added Agent Details

---
+++ BddtagEnforcerTest/BddtagEnforcerTest/AgentDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BddtagEnforcerTest
+{
+    internal class AgentDetails
+    {
+
+        //Added new tags
+        public string BddFeatureMapper = "Loans";
+
+        public void SetAgentDetails()
+        { 
+     
+        }
+    }
+}
