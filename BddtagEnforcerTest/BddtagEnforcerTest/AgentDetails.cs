f07cc30a188ffb0c3e26201219c55fddc3fe59df
abhishekch85 <mail2abhishekch@gmail.com>
2023-09-17 20:19:16 +0530

    Added more tags to map

--- BddtagEnforcerTest/BddtagEnforcerTest/AgentDetails.cs
+++ BddtagEnforcerTest/BddtagEnforcerTest/AgentDetails.cs
@@ -9,8 +9,8 @@ namespace BddtagEnforcerTest
     internal class AgentDetails
     {
 
-        //Added new tags
-        public string BddFeatureMapper = "Loans";
+        //Added new tags over here
+        public string BddFeatureMapper = "Loans,Accounts";
 
         public void SetAgentDetails()
         { 
