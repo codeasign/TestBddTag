2b8357fddbf5bee34bad9ad78aef07f51cdab45e
abhishekch85 <mail2abhishekch@gmail.com>
2023-09-19 14:33:19 +0530

    Added Stock Class

---
+++ BddtagEnforcerTest/BddtagEnforcerTest/StockMarkets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BddtagEnforcerTest
+{
+    internal class StockMarkets
+    {
+
+        public string BddFeatureMapper = "StockIndex,BSE";
+
+        public void ShowDetailsOfStock()
+        { 
+        
+        }
+
+    }
+}
