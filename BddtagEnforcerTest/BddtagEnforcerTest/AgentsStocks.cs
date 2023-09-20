751fe9652b7e5ed3942fbf847110728ee0136e1c
abhishekch85 <mail2abhishekch@gmail.com>
2023-09-20 16:14:30 +0530

    Added AgentStocks

---
+++ BddtagEnforcerTest/BddtagEnforcerTest/AgentsStocks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BddtagEnforcerTest
+{
+    internal class AgentsStocks
+    {
+        public string BddFeatureMapper = "BSE,Broker";
+
+        public void someAnotherFunction()
+        { 
+        }
+    }
+}
